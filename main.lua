d739308974e071972a5da2a47b8eb647f0e9a27f
Jay Roberts <jay@gloryfish.org>
2010-09-29 02:20:40 -0400

    Add pausing, music, sound

--- main.lua
+++ main.lua
@@ -27,12 +27,15 @@ chase.ui = {
   }
 }
 
+chase.paused = false
+
 chase.score = 0
 chase.points = 0
 
 chase.const = {
-  topSpeed = 300,
-  maxArrows = 20,
+  minSpeed = 200,
+  maxSpeed = 300,
+  maxArrows = 10000,
 }
 
 chase.directions = {
@@ -73,7 +76,18 @@ function love.load()
     x = love.mouse.getX(),
     y = love.mouse.getY(),
     l = false,
-  } 
+  }
+  
+  chase.music = love.audio.newSource("resources/music/vibe-ydb.it")
+  chase.music:setLooping(true)
+  
+  chase.sound = {
+    good = love.audio.newSource("resources/sound/good.mp3"),
+    bad = love.audio.newSource("resources/sound/bad.mp3"),
+    click = love.audio.newSource("resources/sound/click.mp3"),
+  }
+  
+  love.audio.play(chase.music)
   
   chase.resume()
 end
@@ -87,54 +101,61 @@ function love.mousepressed(x, y, b) end
 function love.mousereleased(x, y, b) end
 
 function chase.update(dt)
-  if #chase.arrows < chase.const.maxArrows then
-    chase.addArrow()
-  end
+  if not chase.paused then
   
-  local toRemove = {}
+    if #chase.arrows < chase.const.maxArrows then
+      chase.addArrow()
+    end
   
-  for index, arrow in pairs(chase.arrows) do
-    if arrow.state == 'dead' then
-      -- Remove arrows that died during the last update
-      table.insert(toRemove, index)
+    local toRemove = {}
+  
+    for index, arrow in pairs(chase.arrows) do
+      if arrow.state == 'dead' then
+        -- Remove arrows that died during the last update
+        table.insert(toRemove, index)
 
-    elseif arrow:isOffscreen() then
-      -- remove arrows that moved offscreen
-      table.insert(toRemove, index)
+      elseif arrow:isOffscreen() then
+        -- remove arrows that moved offscreen
+        table.insert(toRemove, index)
 
-    elseif arrow:containsPoint( {x = love.mouse.getX(), y = love.mouse.getY()}) and arrow.state == 'normal' then
-      -- Check for mouse hovers
-      if arrow:isInFrontOfPoint(chase.lastMouse) then
-        -- This is a new selection from behind the arrow
-        arrow:setState('selected')
-      else
-        -- This is a failed selection
-        chase.deductPoint()
-        arrow:setState('dying')
+      elseif arrow:containsPoint( {x = love.mouse.getX(), y = love.mouse.getY()}) and arrow.state == 'normal' then
+        -- Check for mouse hovers
+        if arrow:isInFrontOfPoint(chase.lastMouse) then
+          -- This is a new selection from behind the arrow
+          arrow:setState('selected')
+          love.audio.play(chase.sound.click)
+        else
+          -- This is a failed selection
+          chase.deductPoint()
+          arrow:setState('dying')
+        end
+      elseif arrow.state == 'selected' and not arrow:containsPoint( {x = love.mouse.getX(), y = love.mouse.getY()}) then
+        --  Mouse has moved off arrow
+        arrow:setState('normal')
       end
-    elseif arrow.state == 'selected' and not arrow:containsPoint( {x = love.mouse.getX(), y = love.mouse.getY()}) then
-      --  Mouse has moved off arrow
-      arrow:setState('normal')
-    end
 
-    -- Check for clicks
-    if arrow.state == 'selected' and love.mouse.isDown('l') and not chase.lastMouse.l then
-      chase.addPoint()
-      arrow:setState('clicked')
-    end
+      -- Check for clicks
+      if arrow.state == 'selected' and love.mouse.isDown('l') and not chase.lastMouse.l then
+        chase.addPoint()
+        arrow:setState('clicked')
+      end
     
-    arrow:update(dt)
-  end
+      arrow:update(dt)
+    
+    end
   
-  for i, index in pairs(toRemove) do
-    chase.deactivateArrow(index)
-  end
+    for i, index in pairs(toRemove) do
+      chase.deactivateArrow(index)
+    end
   
-  chase.lastMouse = {
-    x = love.mouse.getX(),
-    y = love.mouse.getY(),
-    l = love.mouse.isDown('l'),
-  } 
+    chase.lastMouse = {
+      x = love.mouse.getX(),
+      y = love.mouse.getY(),
+      l = love.mouse.isDown('l'),
+    } 
+
+  else -- Game is paused
+  end
   
 end
 
@@ -142,6 +163,8 @@ end
 function chase.keypressed(key, unicode)
   if (key == "escape") then
     love.event.push('q')
+  elseif (key == "p") then
+    chase.togglePaused()
   end
 end
 
@@ -150,10 +173,14 @@ function chase.mousepressed(x, y, button)
 
 end
 
+function chase.togglePaused()
+  chase.paused = not chase.paused
+end
+
 function chase.addArrow()
   local dir = chase.directions[math.random(1, #chase.directions)]
   local position = chase.getRandomStart[dir]()
-  local speed = math.random(20, chase.const.topSpeed)
+  local speed = math.random(chase.const.minSpeed, chase.const.maxSpeed)
   local arrow = {}
 
   --  Get an inactive arrow, or create a new one
@@ -204,10 +231,14 @@ chase.getRandomStart = {
 function chase.addPoint()
   chase.score = chase.score + 1
   chase.points = chase.points + 1
+
+  love.audio.play(chase.sound.good)
 end
 
 function chase.deductPoint()
   chase.score = chase.score - 1
+
+  love.audio.play(chase.sound.bad)
 end
 
 function chase.draw()
