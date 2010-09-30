45979477ecf3fa9d7f3ba51fff7a473905480ca8
Jay Roberts <jay@gloryfish.org>
2010-09-29 23:28:51 -0400

    Fix title fading

--- scene_title.lua
+++ scene_title.lua
@@ -16,8 +16,12 @@ function title.enter()
   love.graphics.setBackgroundColor(210, 231, 245)
   love.graphics.setFont(fonts.default)
   
-  title.logo = Typeout('arrow_chase', vector(50, love.graphics.getHeight() - 70), 6, fonts.title, {r=0, g=0, b=0, a=200})
-  
+  title.logo = Typeout('arrow_chase', vector(70, love.graphics.getHeight() - 70), 8, fonts.title, {r=0, g=0, b=0, a=200})
+  title.duration = 0
+
+  title.fadeDelay = 1.5
+  title.fadeTime = 4
+  title.fadeAlpha = 0
 end
 
 function title.keypressed(self, key, unicode)
@@ -29,6 +33,7 @@ function title.keypressed(self, key, unicode)
 end
 
 function title.update(self, dt)
+  title.duration = title.duration + dt
   title.logo:update(dt)
 end
 
@@ -36,10 +41,22 @@ function title.draw()
   love.graphics.setColor(0, 0, 0, 150)
 
   title.logo:draw()
-
+  
+  if title.duration < title.fadeDelay then
+    title.fadeAlpha = 0
+  elseif title.duration > title.fadeDelay + title.fadeTime then
+    title.fadeAlpha = 255
+  else
+    title.fadeAlpha = (title.duration - title.fadeDelay) / title.fadeTime * 255
+  end
+   
+  love.graphics.setColor(0, 0, 0, title.fadeAlpha)
+  
   love.graphics.setFont(fonts.default)
-  love.graphics.print("Press the letter g", 
-                      love.graphics.getWidth() / 2 - 100, 
-                      love.graphics.getHeight() / 2);
+
+  love.graphics.print("by jay roberts", 
+                      70, 
+                      love.graphics.getHeight() - 50);
+
 end
 
